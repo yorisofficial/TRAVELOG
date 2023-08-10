1cdc2027f21aac99d149d5776f69328fa0507e10
ADE YOGA WIYADNYANA <adeyogawiyadnyana@stikom-bali.ac.id>
2023-08-11 00:40:25 +0800

    First Upload (Need Update)

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['inter'],
+      },
+      colors: {
+        'primary': '#5D50C6',
+        'primary-hover': '#4A3CBA',
+        'secondary': '#F85E9F',
+      }
+    },
+  },
+  plugins: [],
+}
+
